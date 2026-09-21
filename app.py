11a8863e65fbeb27b260f92b43d911529bdc7c3b
shreyachundi <shreyachundi@gmail.com>
2026-09-21 09:58:47 +0530

    Second commit

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def register():
     return render_template('success.html', name=name, email=email, student_id=student_id)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5003)
     
